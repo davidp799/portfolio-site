f4a0d430f49dc1b7d3cbdf99859e6752cf12a977
David <papdav12@gmail.com>
2022-05-06 12:19:05 -0400

    merge

---
+++ scripts/tell-it-for-me/main.js
@@ -0,0 +1,8 @@
+
+
+var myStory = document.getElementById('storySubmit');
+myStory.onclick = function() {
+	const myHeading =
+	document.querySelector('h1');
+	myHeading.textContent = 'Hello world!';
+}
